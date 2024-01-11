af6fce9d8b01d4bc3fde9e89666b1a32d8613a90
Sik <contact@sikelio.wtf>
2024-01-11 13:59:13 +0100

    Added keys to move player

--- src/main/java/wtf/devops/spaceinvader/MainGui.java
+++ src/main/java/wtf/devops/spaceinvader/MainGui.java
@@ -2,10 +2,14 @@ package wtf.devops.spaceinvader;
 
 import com.almasb.fxgl.app.GameApplication;
 import com.almasb.fxgl.app.GameSettings;
-import javafx.application.Application;
-import javafx.scene.Group;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
+import com.almasb.fxgl.dsl.FXGL;
+import com.almasb.fxgl.entity.Entity;
+import com.almasb.fxgl.input.Input;
+import com.almasb.fxgl.input.UserAction;
+import javafx.scene.input.KeyCode;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Text;
 
 public class MainGui extends GameApplication {
     public static void main(String[] args) {
@@ -18,5 +22,36 @@ public class MainGui extends GameApplication {
         gameSettings.setHeight(600);
         gameSettings.setTitle("Space Invader");
         gameSettings.setVersion("1.0.0");
+    }
+
+    private Entity player;
+
+    @Override
+    protected void initGame() {
+        this.player = FXGL
+                .entityBuilder()
+                .at(300, 300)
+                .view(new Rectangle(25, 25, Color.BLUE))
+                .buildAndAttach();
+    }
+
+    @Override
+    protected void initInput() {
+        FXGL.onKey(KeyCode.RIGHT, () -> {
+            this.player.translateX(5);
+        });
+
+        FXGL.onKey(KeyCode.LEFT, () -> {
+            this.player.translateX(-5);
+        });
+    }
+
+    @Override
+    protected void initUI() {
+        Text textPixels = new Text();
+        textPixels.setTranslateX(50);
+        textPixels.setTranslateY(100);
+
+        FXGL.getGameScene().addUINode(textPixels);
     }
 }

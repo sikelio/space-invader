d7655243ed238b023f9adb170e267131a2f90edb
Bryan <contact.krausch@gmail.com>
2024-01-11 11:49:31 +0100

    feat: enemywave class created

---
+++ src/main/java/wtf/devops/spaceinvader/common/EnemyWave.java
@@ -0,0 +1,35 @@
+package wtf.devops.spaceinvader.common
+
+public class EnemyWave(){
+    private Enemy[] enemywave;
+    private int speed;
+
+    public void EnemyWave(Enemy[] Enemy, speed){
+        this.enemies = enemywave
+        this.speed = speed;
+    }
+
+    private Enemy[] generate(Enemy[] enemies){
+
+    }
+
+    private Enemy[] removeEnemy(Enemy enemy){
+
+    }
+
+    private Enemy[] getEnemyWave(){
+        return this.enemywave;
+    }
+
+    private int getSpeed(){
+        return this.speed;
+    }
+
+    private void setEnemywave(Enemy[] enemies){
+        this.enemywave = enemies;
+    }
+
+    private void setSpeed(int speed){
+        this.speed =  speed;
+    }
+}

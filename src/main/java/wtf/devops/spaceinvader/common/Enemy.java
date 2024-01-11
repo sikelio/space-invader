a078a5b6534f6d8396b4bc1235688738854a7ca0
Bryan <contact.krausch@gmail.com>
2024-01-11 11:32:51 +0100

    feat: enemy class created

---
+++ src/main/java/wtf/devops/spaceinvader/common/Enemy.java
@@ -0,0 +1,45 @@
+package wtf.devops.spaceinvader.common
+
+import wtf.devops.spaceinvader.common.EnemyType
+
+public class Enemy() {
+    private string lifepoint;
+    private EnemyType enemyType;
+    private float x;
+    private float y;
+    private
+
+
+    public void Enemy(float x, float y){
+        this.x = x;
+        this.y = y;
+    }
+
+    private void moove(){
+
+    }
+
+    private void attack(){
+
+    }
+
+    public string getLifepoint(){
+        return this.lifepoint;
+    }
+
+    public float getX(){
+        return this.x;
+    }
+
+    public float getY(){
+        return this.y;
+    }
+
+    public void setX(float x){
+        return this.x = x;
+    }
+
+    public void setY(float y){
+        return this.y = y;
+    }
+}

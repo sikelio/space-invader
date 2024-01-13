0ea50c6509a591cafe18882359dcac4036e53ce1
Bryan Krausch <contact.krausch@gmail.com>
2024-01-13 17:10:21 +0100

    wip: moveEnemies func init

--- src/main/java/wtf/devops/spaceinvader/MainGui.java
+++ src/main/java/wtf/devops/spaceinvader/MainGui.java
@@ -9,9 +9,11 @@ import javafx.scene.input.KeyCode;
 import javafx.scene.text.Text;
 import wtf.devops.spaceinvader.common.*;
 import wtf.devops.spaceinvader.components.*;
+import wtf.devops.spaceinvader.common.Wave;
 
 import static com.almasb.fxgl.dsl.FXGL.*;
 
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Stack;
 
@@ -29,26 +31,16 @@ public class MainGui extends GameApplication {
     }
 
     private Entity player;
-    private Stack<Entity> enemy;
+    private HashSet<Entity> enemies;
 
     @Override
     protected void initGame() {
     	getGameWorld().addEntityFactory(new SpaceInvaderEntityFactory());
         this.player = getGameWorld().spawn("player");
 
-        this.enemy = new Stack<Entity>();
-        enemy.push(getGameWorld().spawn("enemy", new SpawnData(0,0).put("X", 560).put("Y", 525)));
-        enemy.push(getGameWorld().spawn("enemy", new SpawnData(0,0).put("X", 518).put("Y", 550)));
-        enemy.push(getGameWorld().spawn("enemy", new SpawnData(0,0).put("X", 469).put("Y", 500)));
-        enemy.push(getGameWorld().spawn("enemy", new SpawnData(0,0).put("X", 421).put("Y", 450)));
-        enemy.push(getGameWorld().spawn("enemy", new SpawnData(0,0).put("X", 372).put("Y", 400)));
-        enemy.push(getGameWorld().spawn("enemy", new SpawnData(0,0).put("X", 324).put("Y", 350)));
-        enemy.push(getGameWorld().spawn("enemy", new SpawnData(0,0).put("X", 276).put("Y", 300)));
-        enemy.push(getGameWorld().spawn("enemy", new SpawnData(0,0).put("X", 227).put("Y", 250)));
-        enemy.push(getGameWorld().spawn("enemy", new SpawnData(0,0).put("X", 179).put("Y", 200)));
-        enemy.push(getGameWorld().spawn("enemy", new SpawnData(0,0).put("X", 130).put("Y", 150)));
-        enemy.push(getGameWorld().spawn("enemy", new SpawnData(0,0).put("X", 82).put("Y", 100)));
-        enemy.push(getGameWorld().spawn("enemy", new SpawnData(0,0).put("X", 34).put("Y", 50)));
+        Wave enemiesWave = new Wave();
+        this.enemies = enemiesWave.generateWave();
+        enemiesWave.moveEnemies(this.enemies);
     }
 
     @Override
--- src/main/java/wtf/devops/spaceinvader/common/Wave.java
+++ src/main/java/wtf/devops/spaceinvader/common/Wave.java
@@ -1,33 +1,43 @@
 package wtf.devops.spaceinvader.common;
 
+import com.almasb.fxgl.dsl.FXGL;
 import com.almasb.fxgl.entity.Entity;
 import com.almasb.fxgl.entity.GameWorld;
+import com.almasb.fxgl.entity.SpawnData;
 import com.almasb.fxgl.entity.component.Component;
+import javafx.util.Duration;
 
 import java.util.HashSet;
+import java.util.Stack;
 
-import static com.almasb.fxgl.dsl.FXGL.getGameWorld;
-import static com.almasb.fxgl.dsl.FXGL.spawn;
+import static com.almasb.fxgl.dsl.FXGL.*;
 
 public class Wave {
     private HashSet<Entity> enemies = new HashSet<>();
     private int[] xCord = {34, 82, 130, 179, 227, 276, 324, 372, 421, 469, 518, 560};
-    private int[] yCord = {525, 450, 400, 350, 300, 250, 200, 150, 100, 50};
+    private int[] yCord = {525, 475, 425, 375, 325};
 
-    public void generateWave(){
+
+    public HashSet<Entity> generateWave(){
         double delay = 0.3;
 
         for(int i = 0; i < 5; i++) {
-            for (int e = 0; i < 10; i++) {
-                this.enemies.add(spawn("enemy", 50, 50));
+            for (int e = 0; e < 12; e++) {
+
+                    this.enemies.add(getGameWorld().
+                            spawn("enemy",
+                                    new SpawnData(0,0).put("X", this.xCord[e]).put("Y", this.yCord[i])));
+
             }
         }
+        return this.enemies;
+    }
+
+    public void moveEnemies(HashSet<Entity> enemies){
+        FXGL.getGameTimer().runAtInterval(() ->  {
+            for(Entity enemie : enemies){
+                enemie.translateX(5);
+            }
+        }, Duration.seconds(1));
     }
 }
-
-/* private static class WaveComponent extends Component {
-    @Override
-    public void onUpdate(){
-        
-    }
-} */
